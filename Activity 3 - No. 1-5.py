0d172ce60ad758bd608009ed6486f4a458ac1624
krebsintrouble <106054540+krebsintrouble@users.noreply.github.com>
2023-12-09 23:12:54 +0800

    Create Activity 3 - No. 1-5.py

---
+++ Activity 3 - No. 1-5.py
@@ -0,0 +1,25 @@
+i = 0
+while i < 6:
+    i +=1
+    print("Advanced Computer programming")
+
+i = 0
+while i < 7:
+    print(i)
+    i +=1
+
+i=0
+while i<10:
+    print(i)
+    i+=1
+
+i=0
+while i<10:
+    i+=1
+    print(i)
+
+name= input("Enter the name:")
+i=0
+while i<5:
+    print(name)
+    i+=1
